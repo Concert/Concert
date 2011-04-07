dc9c8ae3468df399a76ec7d564559e8d53dde3fc
Colin Sullivan <colinsul@gmail.com>
2011-04-06 21:44:26 -0400

    Made tags get entered when delimiter or tab or enter is pressed

--- concertapp/static/js/lib/components/AutocompleteListInputComponent.js
+++ concertapp/static/js/lib/components/AutocompleteListInputComponent.js
@@ -27,6 +27,9 @@ var AutocompleteListInputComponent = Component.extend(
         
         _.bindAll(this, '_handle_input_keyup');
         inputElement.bind('keyup', this._handle_input_keyup);
+        
+        _.bindAll(this, '_handle_input_blur');
+        inputElement.bind('blur', this._handle_input_blur);
 
         _.bindAll(this, "render");
     },
@@ -46,15 +49,17 @@ var AutocompleteListInputComponent = Component.extend(
         
         var wordSplit = inputElement.val().split(AutocompleteListInputComponent.prototype.TOKEN_SPLITTER);
         
-        /* If another token was created */
-        if(wordSplit.length > 1) {
+        /* If another token was created because a delimiter was entered */
+        if(wordSplit.length > 1
+            /* Or the return key was pressed */
+            || e.keyCode == 13) {
             /* Grab previous token */
             var token = wordSplit[0];
             
             /* Remove everything from inputElement */
-            inputElement.val('');
+            this.inputElement.val('');
             
-            /* If token was fo-real */
+            /* If token was fo-real (not just another delimiter character) */
             if(token.length) {
                 this._handle_new_token(token);
             }
@@ -63,11 +68,44 @@ var AutocompleteListInputComponent = Component.extend(
     },
     
     /**
+     *  Called when there is a blur event on the input element.
+     *
+     *  @param  {Event}    e    The blur event.
+     **/
+    _handle_input_blur: function(e) {
+        var inputElement = this.inputElement;
+        
+        var finalToken = inputElement.val().split(AutocompleteListInputComponent.prototype.TOKEN_SPLITTER);
+        
+        if(finalToken.length) {
+            /* grab final token */
+            var token = finalToken[0];
+            
+            /* remove everything from inputElement */
+            this.inputElement.val('');
+            
+            if(token.length) {
+                this._handle_new_token(token);
+            }
+        }
+        
+    }, 
+    
+    /**
+     *  Called whenever input field is to be analyzed and new tokens are
+     *  to be discovered.
+     **/
+    _look_for_tokens: function() {
+        
+    }, 
+    
+    /**
      *  Handle a new token that was entered in the inputElement.
      *
      *  @param  {String}    token   The token that was entered.
      **/
     _handle_new_token: function(token) {
+        
         return;
     },
 });
