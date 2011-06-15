9519f2aa1827c7ab45142581f0013dbbccef4353
amywieliczka <amywieliczka@gmail.com>
2011-06-15 14:16:45 -0400

    adding pendingUsers property to the Collection model

--- concertapp/collection/models.py
+++ concertapp/collection/models.py
@@ -23,6 +23,7 @@ class Collection(models.Model):
     name = models.CharField(max_length = 100, unique=True)
     admin = models.ForeignKey(User)
     users = models.ManyToManyField(User, related_name = "collections")
+    pendingUsers = models.ManyToManyField(User, related_name = "pending_collections")
 
     def __unicode__(self):
         return str(self.name)
