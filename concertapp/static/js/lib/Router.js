08404833785c8c10f7ea7d5c1bfce47990f6e9e1
Colin Sullivan <colinsul@gmail.com>
2011-06-24 13:08:55 -0400

    Fixed a bug with default route

--- concertapp/static/js/lib/Router.js
+++ concertapp/static/js/lib/Router.js
@@ -105,8 +105,11 @@ var Router = Backbone.Controller.extend(
      **/
     _initialize_routes: function() { 
         
+        /* If no hash, go to #collections */
         _.bindAll(this, '_default_route');
-        this.route('', 'default', '#collections');
+        this.route('', 'default', function() {
+            window.location.assign('#collections');
+        });
         
         _.bindAll(this, '_collections_route');
         this.route('collections', 'collections', this._collections_route);
@@ -138,14 +141,7 @@ var Router = Backbone.Controller.extend(
         
         return;
     }, 
-    
-    /**
-     *  The default route will just redirect to the defaultHash for now.
-     **/
-    _default_route: function() {
-        window.location.assign(this.defaultHash);
-    }, 
-    
+
     /**
      *  Overriding this method so we can pass extra arguments through our
      *  route:* events.
