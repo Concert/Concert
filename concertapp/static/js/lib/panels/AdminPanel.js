b8222ead935e6249d922eb9b6193d240d4a28d6f
amy wieliczka <amywieliczka@gmail.com>
2011-09-04 20:15:21 -0700

    admin panel javascript object

---
+++ concertapp/static/js/lib/panels/AdminPanel.js
@@ -0,0 +1,21 @@
+/**
+ *  @file       EventsPanel.js
+ *
+ *  @author     amy wieliczka <amywieliczka [at] gmail.com>
+ **/
+
+/**
+ *  @class  Panel that shows administrative actions on a collection
+ *  @extends    Panel
+ **/
+var EventsPanel = Panel.extend({
+    _initialize_elements: function() {
+        Panel.prototype._initialize_elements.call(this);
+
+        var params = this.options;
+        
+        var $ = jQuery;
+        
+    },
+    
+});
