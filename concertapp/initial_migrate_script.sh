6c8a5a51fe6492cc0743f9850bc17f848d769a0a
Colin Sullivan <colinsul@gmail.com>
2011-09-25 12:24:17 -0700

    bleh

--- concertapp/initial_migrate_script.sh
+++ concertapp/initial_migrate_script.sh
@@ -1,4 +1,5 @@
 mv initial_data.json initial_data.json.bak
+./manage.py syncdb
 ./manage.py migrate concertapp 0001_initial
 ./manage.py migrate concertapp.audiofile 0001_initial
 ./manage.py migrate concertapp.audiosegment 0001_initial
