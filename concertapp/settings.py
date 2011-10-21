89b7d3fb25c4223f707754301663564a16e6d0f3
Colin Sullivan <colinsul@gmail.com>
2011-10-21 16:07:50 -0700

    Revert "renaming audio to baseaudio to make south happy"
    This reverts commit 7c42c5be23a3172cb8bfb070d78e43d27c93c641.

--- concertapp/settings.py
+++ concertapp/settings.py
@@ -103,7 +103,7 @@ INSTALLED_APPS = (
     'south',
     'djcelery',
     'concertapp',
-    'concertapp.baseaudio',
+    'concertapp.audio',
     'concertapp.audiofile',
     'concertapp.audiosegment',
     'concertapp.collection',
